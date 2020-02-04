674f8d923d9ad5549c134746340d24c8a51136c9
Kerry Gougeon <kerrygougeon@gmail.com>
2020-02-04 17:50:16 -0500

    Initial Java Code

---
+++ src/Driver.java
@@ -0,0 +1,9 @@
+import java.net.InetAddress;
+import java.net.Socket;
+
+class Driver {
+    public static void main(String[] args) {
+        InetAddress web = InetAddress.getByName("asd");
+
+    }
+}
