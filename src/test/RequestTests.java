a437c3471cd61019e32ef4b906861a9c7f946f6d
Kerry Gougeon <kerrygougeon@gmail.com>
2020-02-05 00:25:19 -0500

    Set up mvn and tests

---
+++ src/test/RequestTests.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+public class RequestTests {
+
+    @Test
+    void genericTests(){
+        assertEquals(2,2,"1+1 should equals 2");
+    }
+}
