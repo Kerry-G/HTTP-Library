2e7f61fb75cd5328f6de3b790d00730f7e5d899f
Kerry Gougeon <kerrygougeon@gmail.com>
2020-04-02 21:16:35 -0400

    Interfaces

---
+++ src/main/java/http/PacketHandler.java
@@ -0,0 +1,14 @@
+package http;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.net.InetAddress;
+import java.util.List;
+
+public class PacketHandler {
+
+    static List<Packet> createPacketList(String serialized, InetAddress inetAddress, int port){
+        throw new NotImplementedException();
+    }
+
+}
